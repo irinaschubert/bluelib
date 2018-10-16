d343c3dada98e23219073f8f0ee75f090baa7326
Ueli <ulrich.schmutz@students.ffhs.ch>
2018-10-16 22:18:03 +0200

    Connection-Klassen für die DB erstellt. Mysql-Connector muss als Library installiert werden.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,15 @@
 package Main;
 
+import java.sql.SQLException;
+
+import DAO.DBConnection;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Welt");
+		
 	}
 
 }
