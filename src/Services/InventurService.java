c62b907ff4dc92d8e8bf1786ce49fcc5c69e8723
irina <irina.schubert@students.ffhs.ch>
2018-10-17 14:57:37 +0200

    add HashRechner with JUnit test class; add InventurService

--- src/Services/InventurService.java
+++ src/Services/InventurService.java
@@ -1,10 +1,30 @@
 package Services;
 
+import java.util.Date;
+import java.util.List;
+import Domain.Person;
+import Domain.Medium;
+import Domain.Inventur;
+
 /**
  * @version 0.1 16.10.2018
  * @author irina
  *
  */
 public class InventurService {
+	
+	public InventurService() {}
+	
+	public Verifikation neueInventur(Date datum, String bezeichnung, Person mitarbeiter) {
+		return Verifikation;
+	}
+	
+	public List<Medium> inventurdifferenz(Inventur inventur){
+		return List;
+	}
+	
+	public Verifikation mediumBuchen(Medium medium) {
+		return Verifikation;
+	}
 
 }
---
+++ src/unitTests/HashRechnerTest.java
@@ -0,0 +1,24 @@
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import Services.HashRechner;
+
+import org.junit.jupiter.api.Test;
+
+class HashRechnerTest {
+
+	@Test
+	void test() {
+		String resultat;
+		String eingabe;
+		String md5;
+		
+		eingabe = "hallo";
+		md5 = "598d4c200461b81522a3328565c25f7c";
+		HashRechner hashRechner = new HashRechner();
+		
+		resultat = hashRechner.hashBerechnen(eingabe);
+		assertEquals(md5, resultat);
+	}
+
+}
