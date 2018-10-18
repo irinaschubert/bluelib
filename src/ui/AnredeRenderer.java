bc182adb498dcb34be9877e499ee0535fb410d3e
Ueli <ulrich.schmutz@students.ffhs.ch>
2018-10-18 15:50:08 +0200

    Vereinfachung des Casts im AnredeRenderer, Hinzufügen von Kommentaren

--- src/ui/AnredeRenderer.java
+++ src/ui/AnredeRenderer.java
@@ -7,15 +7,25 @@ import javax.swing.plaf.basic.BasicComboBoxRenderer;
 
 import domain.Anrede;
 
+/**
+ * 
+ * Mit dem Renderer können die ComboBox-Inhalte aus den Domänenobjekten flexibel zusammengestellt werden
+ * 
+ * @version 1.0 18.10.2018
+ * @author Schmutz
+ *
+ */
+
 public class AnredeRenderer extends BasicComboBoxRenderer{
 	@Override
 	  public Component getListCellRendererComponent(JList list, Object value,
 	      int index, boolean isSelected, boolean cellHasFocus) {
 	    super.getListCellRendererComponent(list, value, index, isSelected,
 	        cellHasFocus);
+//	    Definition der Variablen aus dem Objekt, welche in der ComboBox angezeigt werden sollen
+//	    Der Rückgabewert sollte vom Datentyp String sein. Der Cast des Objekts zu Anrede ist leidernötig
 	    if (value != null) {
-	      Anrede anrede = (Anrede) value;
-	      setText(anrede.getBezeichnung());
+	      setText(((Anrede) value).getBezeichnung());
 	    }
 	    return this;
 	  }
