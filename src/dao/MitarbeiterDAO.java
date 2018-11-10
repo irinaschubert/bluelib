731240514565c450354fb5eaceca064ed4c8da6c
Ueli <ulrich.schmutz@students.ffhs.ch>
2018-11-10 21:36:15 +0100

    Aufbau Login und MA-Objekt

--- src/dao/MitarbeiterDAO.java
+++ src/dao/MitarbeiterDAO.java
@@ -1,16 +1,16 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 import domain.Mitarbeiter;
-import hilfsklassen.DateConverter;
+import hilfsklassen.SQLHelfer;
 import interfaces.DAOInterface;
+import services.HashRechner;
 /**
  * @version 0.1 28.10.2018
  * @author Mike
@@ -18,6 +18,17 @@ import interfaces.DAOInterface;
  */
 public class MitarbeiterDAO implements DAOInterface<Mitarbeiter> {
 
+	private DBConnection dbConnection = null;
+	private Connection conn = null; 
+	private PreparedStatement pstmt = null;
+	private List<Mitarbeiter> mitarbeiterListe = null;
+	
+	
+	public MitarbeiterDAO() {
+		mitarbeiterListe = new ArrayList<>();
+		dbConnection = DBConnection.getInstance();
+	}
+	
 	@Override
 	public Mitarbeiter save(Mitarbeiter domainObject) {
 		// TODO Auto-generated method stub
@@ -38,8 +49,77 @@ public class MitarbeiterDAO implements DAOInterface<Mitarbeiter> {
 
 	@Override
 	public List<Mitarbeiter> getSelektion(Mitarbeiter domainObject) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResultSet rs = null;
+		String sql = "SELECT "
+				+ "id, "
+				+ "benutzername, "
+				+ "passwort, "
+				+ "admin, "
+				+ "aktiv "
+				+ "FROM mitarbeiter ";
+		
+				// Admin-Flag wird immer abgefragt, daher mit WHERE
+				sql = sql + ("WHERE admin = ? ");		
+				
+				if (domainObject.getBenutzername() != null) {
+					sql = sql + "AND benutzername";
+					sql = sql + (SQLHelfer.likePruefung(domainObject.getBenutzername())?" LIKE": " =");
+					sql = sql + " ?";
+				}
+				// Passwort darf nie mit Wildcard abgefragt werden
+				if (domainObject.getPasswort() != null) {
+					sql = sql + ("AND passwort = ? ");
+				}
+				
+				// Status ist immer gesetzt
+				sql = sql + ("AND aktiv = ?");
+	
+			try {
+				
+				int pCounter = 1;
+				conn = dbConnection.getDBConnection();
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setBoolean(pCounter++, domainObject.isAdmin());
+				;
+				
+				if (domainObject.getVorname() != null) {
+					pstmt.setString(pCounter++,SQLHelfer.SternFragezeichenErsatz(domainObject.getBenutzername()));
+					
+				}
+				if (domainObject.getPasswort() != null) {
+					pstmt.setString(pCounter++,HashRechner.hashBerechnen(domainObject.getBenutzername()));
+					
+				}
+				pstmt.setBoolean(pCounter++, domainObject.isAktiv());
+				
+
+				rs = pstmt.executeQuery();
+				pCounter = 0;
+				while(rs.next()) {
+					 Mitarbeiter m = new Mitarbeiter();
+					 m.setId(rs.getInt(pCounter++));
+					 m.setBenutzername(rs.getString(pCounter++));
+					 m.setPasswort(rs.getString(pCounter++));
+					 m.setAdmin(rs.getBoolean(pCounter++));
+					 m.setAktiv(rs.getBoolean(pCounter++));
+					 mitarbeiterListe.add(m);
+					 }
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+	
+
+				   } finally{
+				         try{
+				             if(rs != null) rs.close();
+				             if(pstmt != null) pstmt.close();
+				             if(conn != null) conn.close();
+				         } catch(Exception ex){}
+				     }
+			
+			return mitarbeiterListe;
 	}
 
 	@Override
@@ -52,6 +132,44 @@ public class MitarbeiterDAO implements DAOInterface<Mitarbeiter> {
 	public List<Mitarbeiter> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Boolean loginPruefung(String benutzername, String passwort) {
+		Boolean darfEinloggen = false; 
+		ResultSet rs = null;
+		String sql = "SELECT "
+				+ "id "
+				+ "FROM mitarbeiter "
+				+ "WHERE aktiv = TRUE "
+				+ "AND benutzername = ? "
+				+ "AND passwort = ?";
+	
+			try {
+				
+				int pCounter = 1;
+				conn = dbConnection.getDBConnection();
+				pstmt = conn.prepareStatement(sql);
+				
+				pstmt.setString(pCounter++, benutzername);
+				pstmt.setString(pCounter++, HashRechner.hashBerechnen(passwort));
+
+				rs = pstmt.executeQuery();
+				darfEinloggen = (rs.next()?true:false);
+				
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+	
+
+				   } finally{
+				         try{
+				             if(rs != null) rs.close();
+				             if(pstmt != null) pstmt.close();
+				             if(conn != null) conn.close();
+				         } catch(Exception ex){}
+				     }
+			
+			return darfEinloggen;
 	}
 	
 }
--- src/services/LoginService.java
+++ src/services/LoginService.java
@@ -1,5 +1,8 @@
 package services;
 
+import dao.AutorDAO;
+import dao.MitarbeiterDAO;
+
 /**
  * @version 0.1 16.10.2018
  * @author irina
@@ -11,6 +14,15 @@ public class LoginService {
 	
 	public Verifikation loginPruefen(String name, String pw) {
 		Verifikation v = new Verifikation();
+		if (new MitarbeiterDAO().loginPruefung(name, pw)) {
+			v.setAktionErfolgreich(true);
+			v.setNachricht("Willkommmen");
+		}
+		else {
+			v.setAktionErfolgreich(false);
+			v.setNachricht("Das Login war nicht erfolgreich");
+		}
+		
 		return v;
 	}
 }
