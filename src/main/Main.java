bb9bbb219c15f3aa608d859637335611589b2e08
Michael Trösch <michael.troesch@students.ffhs.ch>
2018-11-13 18:49:01 +0100

    merge bibliotheksdaten

--- src/main/Main.java
+++ src/main/Main.java
@@ -37,7 +37,7 @@ public class Main {
 	            	new BibliothekController(view);	                
 	            }
 	    });  
-/*
+
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -46,7 +46,7 @@ public class Main {
 
 			}
 		});
-
+/*
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
